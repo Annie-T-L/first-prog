eb1d157489607ef6e8cc9177a88f1ac4ef3793e1
Annie-T-L <annatelle13@ukr.net>
2016-05-19 14:50:29 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello  " << name << ", my friend!" << std::endl;
+
+}
+
